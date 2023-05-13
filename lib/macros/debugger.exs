d22ab744f1ea9c3043c9fae4fc42bb801b0a1e2c
Kenneth Kostrešević <kenneth.kostresevic@gmail.com>
2023-05-13 14:24:02 +0200

    Fix log macro by using bind_quoted instead of unquoting twice

--- lib/macros/debugger.exs
+++ lib/macros/debugger.exs
@@ -1,11 +1,11 @@
 defmodule Debugger do
   defmacro log(expression) do
     if Application.get_env(:debugger, :log_level) == :debug do
-      quote do
+      quote bind_quoted: [expression: expression] do
         IO.puts("=============")
-        IO.inspect(unquote(expression))
+        IO.inspect(expression)
         IO.puts("=============")
-        unquote(expression)
+        expression
       end
     else
       expression
